288a7ffd310a2ef6a80956cf6ec54a2ea158c2e2
Mezni <mamezni@gmail.com>
2023-12-16 19:46:25 -0500

    work 20231216

--- finops/core.py
+++ finops/core.py
@@ -1,68 +1,31 @@
-import yaml, random
-from datetime import datetime, timedelta
+import boto3
 
 
-class ContextManager:
-    def __init__(self, account_config) -> None:
-        self.start_time = datetime.now()
-        self.end_time = ""
+class CostAws:
+    def __init__(self, credentials, params) -> None:
+        self.credentials = credentials
+        self.params = params
         self.status = "success"
         self.message = ""
-        self.check_config(account_config)
 
-    def check_config(self, account_config) -> None:
-        for attr in ["client_name", "client_code", "cloud_name"]:
-            if account_config.get(attr):
-                setattr(self, attr, account_config.get(attr))
+    def create_connection(self):
+        for attr in ["access_key_id", "secret_access_key", "region"]:
+            if self.credentials.get(attr):
+                setattr(self, attr, self.credentials.get(attr))
             else:
                 self.status = "failed"
-                self.message = "attribute " + attr + " is mandatory, in config file"
-
-    def set_end_time(self) -> None:
-        self.end_time = datetime.now()
-
-    def get_state(self) -> dict:
-        state = {
-            "execution": {
-                "start_time": self.start_time.strftime("%d-%m-%Y %H:%M:%S"),
-                "end_time": self.end_time.strftime("%d-%m-%Y %H:%M:%S"),
-                "status": self.status,
-                "message": self.message,
-            },
-            "query_params": {
-                "start_date": "",
-                "end_date": "",
-                "granurality": "",
-                "dimensions": "",
-            },
-        }
-        return state
-
-
-class ConfigManager:
-    def __init__(self, config_file) -> None:
-        self.config_file = config_file
-        self.config_data = self.load_config()
-        self.accounts = self.get_accounts()
-
-    def load_config(self) -> dict:
-        try:
-            with open(self.config_file, "r") as file:
-                data = yaml.safe_load(file)
-        except:
-            data = {}
-        return data
-
-    def get_accounts(self) -> list:
-        accounts = []
-        try:
-            for acc in self.config_data["clients"]:
-                account = {
-                    "client_name": acc["client_name"],
-                    "client_code": acc["client_code"],
-                    "cloud_name": acc["cloud_name"],
-                }
-                accounts.append(account)
-        except:
-            pass
-        return accounts
+                self.message = (
+                    "create connection : "
+                    + "attribute "
+                    + attr
+                    + " is mandatory, in config file"
+                )
+        if self.status == "failed":
+            return boto3.client(
+                "ce",
+                aws_access_key_id=self.access_key_id,
+                aws_secret_access_key=self.secret_access_key,
+                region_name=self.region,
+            )
+        else:
+            return None
