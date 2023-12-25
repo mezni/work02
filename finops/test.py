079494cc02e80808d4e5fb460fc6eb5fbc208a51
Mezni <mamezni@gmail.com>
2023-12-25 17:50:31 -0500

    work 20231225

--- finops/test.py
+++ finops/test.py
@@ -1,64 +1,63 @@
-import uuid
+import yaml, json, uuid
 from datetime import datetime
-
-__author__ = "Mohamed Ali MEZNI"
-__version__ = "2023-12-24"
 
 
 class ContextManager:
-    def __init__(self, config, last_state, prev_state) -> None:
-        self.context_id = uuid.uuid4()
+    def __init__(self) -> None:
+        self.context_id = str(uuid.uuid4)
         self.start_time = datetime.now()
         self.end_time = None
         self.status = "success"
         self.message = ""
-        self.config = config
-        self.last_state = last_state
-        self.prev_state = prev_state
-        self.execution = self.set_execution()
-        self.variables = self.set_variables()
-        self.params = self.set_params()
+        self.data = [1, 2, 3]
 
-    def set_execution(self) -> dict:
-        self.end_time = datetime.now()
-        execution = {
-            "context_id": str(self.context_id),
-            "start_time": self.start_time.strftime("%d-%m-%Y %H:%M:%S"),
-            "end_time": self.end_time.strftime("%d-%m-%Y %H:%M:%S"),
-            "status": self.status,
-            "message": self.message,
-        }
-        return execution
-
-    def set_variables(self) -> dict:
-        return {}
-
-    def set_params(self) -> dict:
-        params = {
-            "start_date": "",
-            "end_date": "",
-            "granularity": "DAILY",
-            "dimensions": ["LINKED_ACCOUNT", "SERVICE"],
-            "metrics": ["BlendedCost"],
-            "filters": "",
-        }
-        return params
-
-    def get_context(self) -> dict:
-        context = {"execution": self.execution, "params": self.params}
-        return context
-
-    def get_curr_state(self) -> dict:
-        state = {}
-        return state
-
-    def get_prev_state(self) -> dict:
-        state = {}
-        return state
+    def __iter__(self):
+        return iter(self.data)
 
 
-config = {"client_name": "client1", "client_code": "client1"}
-last_state = {}
-prev_state = {}
-context = ContextManager(config, last_state, prev_state)
-print(context.get_context())
+ctx = ContextManager()
+for c in ctx:
+    print(c)
+
+
+class ConfigManager:
+    def __init__(self, config_file) -> None:
+        self.config_file = config_file
+        self.config_data = self.load_config()
+        self.accounts = self.get_accounts()
+
+    def load_config(self) -> dict:
+        try:
+            with open(self.config_file, "r") as file:
+                data = yaml.safe_load(file)
+        except:
+            data = {}
+        return data
+
+    def get_config(self) -> list:
+        return self.config_data["app"]
+
+    def get_accounts(self) -> list:
+        accounts = []
+        try:
+            for acc in self.config_data["clients"]:
+                account = {
+                    "client_name": acc["client_name"],
+                    "client_code": acc["client_code"],
+                    "cloud_name": acc["cloud_name"],
+                    "account_name": acc["account_name"],
+                    "access_key_id": acc["access_key_id"],
+                    "secret_access_key": acc["secret_access_key"],
+                }
+                accounts.append(account)
+        except:
+            pass
+        return accounts
+
+    def __iter__(self):
+        return iter(self.accounts)
+
+
+cfg = ConfigManager("config.yaml")
+for c in cfg:
+    print(c)
