88ffdb98406f2e2b3a691ead90cfc50e5d67b16b
Mezni <mamezni@gmail.com>
2023-12-26 11:13:05 -0500

    work 20231226

--- finops/cost_data.py
+++ finops/cost_data.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    KEY_VAULT_NAME: str
     BRONZE_CONTAINER: str
     SILVER_CONTAINER: str
 
@@ -30,6 +31,34 @@ class StorageManager:
     def get_content(self):
         return {}
 
+    def upload_data(self, data):
+        pass
+
+
+class ContextManager:
+    def __init__(self, account, last_state, prev_state) -> None:
+        self.cloud_name = "aws"
+        pass
+
+    def get_states(self):
+        return {}, {}
+
+    def get_context(self):
+        return {}
+
+    def exit(self):
+        pass
+
+
+class CostAws:
+    def __init__(self, context) -> None:
+        pass
+
+
+class CostAzure:
+    def __init__(self, context) -> None:
+        pass
+
 
 # MAIN
 settings = Settings()
@@ -43,3 +72,13 @@ for account_cfg in config.get_accounts():
     print(account_cfg)
     last_state = storage_mgr.get_content()
     prev_state = storage_mgr.get_content()
+    ctx = ContextManager(account_cfg, last_state, prev_state)
+    if ctx.cloud_name == "aws":
+        cost_data = CostAws(ctx.get_context())
+    if ctx.cloud_name == "azure":
+        cost_data = CostAzure(ctx.get_context())
+    ctx.exit()
+    storage_mgr.upload_data(cost_data)
+    current_state, last_state = ctx.get_states()
+    storage_mgr.upload_data(current_state)
+    storage_mgr.upload_data(last_state)
