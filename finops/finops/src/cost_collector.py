b0bf87de6754dcc6cb63c3f1d6e178338fc1e45a
Mezni <mamezni@gmail.com>
2024-01-05 09:17:13 -0500

    work 20240105

--- finops/finops/src/cost_collector.py
+++ finops/finops/src/cost_collector.py
@@ -1,8 +1,13 @@
 __author__ = "Mohamed Ali MEZNI"
 __version__ = "2024-01-05"
 
-from cost_core import Settings
+from cost_core import Settings, ConfigManager
 
 settings, status = Settings().get_settings()
+if not status["error"]:
+    print(status["message"])
+else:
+    pass
 
-print(settings)
+config = ConfigManager(settings["config_file_name"])
+print(config.get_accounts())
--- finops/finops/src/cost_core.py
+++ finops/finops/src/cost_core.py
@@ -8,6 +8,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     CONFIG_FILE_NAME: str
     KEY_VAULT_NAME: str
+    STORAGE_ACCOUNT_NAME: str
     BRONZE_CONTAINER: str
     SILVER_CONTAINER: str
 
@@ -17,13 +18,52 @@ class Settings(BaseSettings):
         status = {"error": False, "message": ""}
         try:
             settings = {
+                "config_file_name": self.CONFIG_FILE_NAME,
                 "key_vault_name": self.KEY_VAULT_NAME,
+                "storage_account_name": self.STORAGE_ACCOUNT_NAME,
                 "bronze_container": self.BRONZE_CONTAINER,
                 "silver_container": self.SILVER_CONTAINER,
-                "config_file_name": self.CONFIG_FILE_NAME,
             }
         except:
             settings = {}
             status["error"] = True
             status["message"] = "get settings"
         return settings, status
+
+
+class ConfigManager:
+    def __init__(self, config_file) -> None:
+        self.status = {}
+        self.config_file = config_file
+        self.config_data = self.load_config()
+
+    def load_config(self) -> dict:
+        status = {"error": False, "message": ""}
+        try:
+            with open(self.config_file, "r") as file:
+                data = yaml.safe_load(file)
+        except:
+            data = {}
+            status["error"] = True
+            status["message"] = "load config"
+            self.status = status
+        return data
+
+    def get_accounts(self) -> list:
+        status = {"error": False, "message": ""}
+        accounts = []
+        try:
+            for acc in self.config_data["clients"]:
+                account = {
+                    "client_name": acc["client_name"],
+                    "cloud_name": acc["cloud_name"],
+                    "account_name": acc["account_name"],
+                    "access_key_id": acc["access_key_id"],
+                    "secret_access_key": acc["secret_access_key"],
+                }
+                accounts.append(account)
+        except:
+            status["error"] = True
+            status["message"] = "load config"
+
+        return accounts, status
