072eaa7ae9ed320257130a09c487775215c9ce2a
Mezni <mamezni@gmail.com>
2024-01-09 11:26:05 -0500

    work 20240109

--- finops/finops/src/cost_collector_v1.py
+++ finops/finops/src/cost_collector_v1.py
@@ -8,8 +8,12 @@ class CostAws:
         self.start_time = datetime.now()
         self.end_time = ""
         self.context_id = str(uuid.uuid4())
-        self.output_file_name = ""
+        self.error = False
+        self.message = ""
         self.config = config
+        self.start_date = ""
+        self.end_date = ""
+        self.output_file_name = ""
         self.ce_client = self.create_client()
 
     def create_client(self):
@@ -21,18 +25,32 @@ class CostAws:
                 region_name=self.config["region"],
             )
         except Exception as e:
+            self.error = True
+            self.message = "Cannot create AWS client"
+            self.end_time = datetime.now()
             return None
 
-    def get_context(self):
+    def get_state(self):
         context = {
-            "context_id": self.context_id,
-            "start_time": self.start_time,
-            "end_time": self.end_time,
+            "execution": {
+                "context_id": self.context_id,
+                "start_time": self.start_time.strftime("%d-%m-%Y %H:%M:%S"),
+                "end_time": self.end_time.strftime("%d-%m-%Y %H:%M:%S"),
+                "error": self.error,
+                "error": self.message,
+                "output_file": self.output_file_name,
+            },
+            "params": {
+                "start_date": self.start_date,
+                "end_date": self.end_date,
+                "granularity": "DAILY",
+                "dimensions": ["LINKED_ACCOUNT", "SERVICE"],
+                "filter": "",
+            },
         }
         return context
 
     def get_query_dates(self):
-        last_start_date = self.config["last_start_date"]
         last_end_date = self.config["last_end_date"]
         end_date = datetime.now().strftime("%Y-%m-%d")
         if last_end_date == "":
@@ -44,18 +62,17 @@ class CostAws:
             start_date = last_end_date
         if start_date[5:7] != end_date[5:7]:
             start_date = start_date[:-2] + "01"
-        return start_date, end_date
+        self.start_date = start_date
+        self.end_date = end_date
 
     def get_cost_data(self):
-        status = {"error": False, "message": ""}
         results = []
         token = None
-        if not self.ce_client:
-            status["error"] = True
-            status["message"] = "Cannot get AWS client"
-        else:
+        if not self.error:
             try:
-                start_date, end_date = self.get_query_dates()
+                self.get_query_dates()
+                start_date = self.start_date
+                end_date = self.end_date
                 while True:
                     if token:
                         kwargs = {"NextPageToken": token}
@@ -76,15 +93,16 @@ class CostAws:
                     if not token:
                         break
             except:
-                status["error"] = True
-                status["message"] = "Cannot generate AWS data"
-        return results, status
+                self.error = True
+                self.message = "Cannot generate AWS data"
+                self.end_time = datetime.now()
+        return results
 
     def generate_csv(self):
-        status = {"error": False, "message": ""}
         columns = [
             "periode",
             "client",
+            "cloud",
             "compte",
             "service",
             "cout",
@@ -102,7 +120,7 @@ class CostAws:
             output_dir
             + "/"
             + "finops_"
-            + client_code
+            + client_name
             + "_"
             + cloud_name
             + "_"
@@ -112,38 +130,43 @@ class CostAws:
             + ".csv"
         )
         self.output_file_name = output_file_name
-        cost_data, s = self.get_cost_data()
 
-        if s["error"] == []:
-            status["error"] = True
-            status["message"] = s["message"]
-        else:
-            with open(self.output_file_name, "w") as file:
-                file.write(",".join(columns) + "\n")
-                for result_by_time in cost_data:
-                    for group in result_by_time["Groups"]:
-                        period = result_by_time["TimePeriod"]["Start"]
-                        account = group["Keys"][0]
-                        service = group["Keys"][1]
-                        amount = group["Metrics"]["UnblendedCost"]["Amount"]
-                        unit = group["Metrics"]["UnblendedCost"]["Unit"]
-                        estimated = result_by_time["Estimated"]
-                        line = (
-                            period
-                            + ","
-                            + client_code
-                            + ","
-                            + account
-                            + ","
-                            + service
-                            + ","
-                            + amount
-                            + ","
-                            + unit
-                            + ","
-                            + str(estimated)
-                        )
-                        file.write(line + "\n")
+        cost_data = self.get_cost_data()
+        if not self.error:
+            try:
+                with open(self.output_file_name, "w") as file:
+                    file.write(",".join(columns) + "\n")
+                    for result_by_time in cost_data:
+                        for group in result_by_time["Groups"]:
+                            period = result_by_time["TimePeriod"]["Start"]
+                            account = group["Keys"][0]
+                            service = group["Keys"][1]
+                            amount = group["Metrics"]["UnblendedCost"]["Amount"]
+                            unit = group["Metrics"]["UnblendedCost"]["Unit"]
+                            estimated = result_by_time["Estimated"]
+                            line = (
+                                period
+                                + ","
+                                + client_code
+                                + ","
+                                + account
+                                + ","
+                                + service
+                                + ","
+                                + amount
+                                + ","
+                                + unit
+                                + ","
+                                + str(estimated)
+                            )
+                            file.write(line + "\n")
+            except:
+                self.error = True
+                self.message = "Cannot generate file"
+            self.end_time = datetime.now()
+
+        state = self.get_state()
+        return state
 
 
 account_cfg = {
@@ -160,5 +183,8 @@ account_cfg = {
 
 # MAIN
 query_history_days = 180
-cost_aws = CostAws(account_cfg)
-cost_aws.generate_csv()
+
+if account_cfg["cloud_name"] == "aws":
+    cost_aws = CostAws(account_cfg)
+    state = cost_aws.generate_csv()
+    print(state)
