847e9a238f4334c8e882f8266a94c075d77ed733
Mezni <mamezni@gmail.com>
2024-01-12 09:41:20 -0500

    work 20240112

--- finops/finops/src/cost_merger.py
+++ finops/finops/src/cost_merger.py
@@ -52,34 +52,3 @@ if status["error"]:
 first_load = True
 df_result = None
 input_files = storage_mgr.list_blobs(bronze_container)
-for file_name in input_files:
-    if file_name.startswith("finops"):
-        storage_mgr.download_blob(
-            bronze_container,
-            file_name,
-            tmp_dir + "/" + file_name,
-        )
-        df = pd.read_csv(tmp_dir + "/" + file_name)
-        if first_load:
-            df_result = df
-            first_load = False
-        else:
-            df_result = pd.concat([df_result, df])
-if df_result:
-    df_result = df_result.drop_duplicates()
-
-    storage_mgr.download_blob(
-        silver_container,
-        finops_file_name,
-        tmp_dir + "/" + finops_file_name,
-    )
-    try:
-        df_finops = pd.read_csv(tmp_dir + "/" + finops_file_name)
-        df_finops = pd.concat([df_finops, df_result])
-    except:
-        df_finops = df_result
-
-    df_finops = df_finops.drop_duplicates(subset=duplicate_cols, keep="last")
-    df_finops.to_csv(tmp_dir + "/" + finops_file_new_name, index=False)
-    storage_mgr.upload_blob(silver_container, finops_file_new_name, finops_file_name)
-logger.info("End")
