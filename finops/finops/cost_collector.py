d44cfd21c452181740760a8abc490a969f55d9c0
Mezni <mamezni@gmail.com>
2024-02-04 10:52:09 -0500

    work 20240204

--- finops/finops/cost_collector.py
+++ finops/finops/cost_collector.py
@@ -1,8 +1,9 @@
 __author__ = "Mohamed Ali MEZNI"
 __version__ = "2024-02-05"
 
-import os, sys, logging
-from cost_core import Settings, ConfigManager
+import os, sys, logging, uuid, json
+from datetime import datetime, timedelta
+from cost_core import Settings, ConfigManager, StorageManager, VaultManager
 
 
 def get_logger():
@@ -30,7 +31,9 @@ def get_secrets(account):
             sec = {cre.get("key", ""): cre.get("value", "")}
             secrets.append(sec)
         elif cre.get("store", "") == "keystore":
-            sec = {cre.get("key", ""): cre.get("value", "")}
+            secret_key = cre.get("value", "")
+            secret_value = keyvault_mgr.get_secret(secret_key)
+            sec = {cre.get("key", ""): secret_value}
             secrets.append(sec)
         else:
             pass
@@ -39,10 +42,76 @@ def get_secrets(account):
     return account
 
 
+def get_state(state_file_name):
+    storage_mgr.download_blob(
+        bronze_container,
+        "logs/" + state_file_name,
+        tmp_dir + "/" + state_file_name,
+    )
+
+    try:
+        with open(tmp_dir + "/" + state_file_name) as fp:
+            state = json.load(fp)
+    except:
+        state = {}
+    return state
+
+
+def get_dates(account):
+    last_start_date = ""
+    last_end_date = ""
+    last_state = get_state(account["last_state_file_name"])
+    prev_state = get_state(account["prev_state_file_name"])
+    try:
+        if not last_state["execution"]["error"]:
+            last_start_date = last_state["params"]["start_date"]
+            last_end_date = last_state["params"]["end_date"]
+        else:
+            last_start_date = prev_state["params"]["start_date"]
+            last_end_date = prev_state["params"]["end_date"]
+    except:
+        pass
+
+    account["last_start_date"] = last_start_date
+    account["last_end_date"] = last_end_date
+    return account
+
+
+class CostAzure:
+    def __init__(self, config) -> None:
+        self.start_time = datetime.now()
+        self.end_time = ""
+        self.context_id = str(uuid.uuid4())
+        self.error = False
+        self.message = ""
+        self.config = config
+        self.end_date = datetime.now().strftime("%Y-%m-%d")
+        self.start_date = self.get_query_start_date()
+        self.output_file_name = ""
+        self.ce_client = self.create_client()
+
+    def get_query_start_date(self):
+        last_end_date = self.config["last_end_date"]
+        if last_end_date == "":
+            date_history = (
+                datetime.utcnow() - timedelta(days=query_history_days)
+            ).strftime("%Y-%m-%d")
+            start_date = date_history[:-2] + "01"
+        else:
+            start_date = last_end_date
+        if start_date[5:7] != self.end_date[5:7]:
+            start_date = start_date[:-2] + "01"
+        return start_date
+
+    def create_client(self):
+        return None
+
+
 # Main
 env_file = "env"
 clients_file = "clients.yaml"
-
+tmp_dir = "/tmp"
+query_history_days = 90
 
 logger = get_logger()
 logger.info("Debut")
@@ -53,21 +122,33 @@ if not settings:
     logger.error(f"cannot read settings")
     logger.info("End")
     sys.exit(1)
+
 config = ConfigManager(clients_file)
 if not config:
     logger.error(f"cannot read clients config")
     logger.info("End")
     sys.exit(1)
 
+bronze_container = settings["bronze_container"]
+silver_container = settings["silver_container"]
+storage_mgr = StorageManager(
+    settings["storage_account_name"], settings["storage_account_key"]
+)
+
+keyvault_mgr = VaultManager(settings["key_vault_name"])
+
 accounts = config.get_accounts()
 for account in accounts:
     logger.info(
         f"-> traitement client=<{account['client']}>  account=<{account['account']}>"
     )
-    account = get_secrets(account)
+    account_conf = get_secrets(account)
+    account_conf = get_dates(account_conf)
+    print(account_conf)
     if account["cloud"] == "aws":
         logger.info(f"   status=SUCCESS")
     elif account["cloud"] == "azure":
+        cost_data = CostAzure(account_conf)
         logger.info(f"   status=SUCCESS")
     else:
         logger.info(f"   status=FAIL  cloud <{account['cloud']}> non implemente")
--- finops/finops/cost_core.py
+++ finops/finops/cost_core.py
@@ -2,8 +2,11 @@ __author__ = "Mohamed Ali MEZNI"
 __version__ = "2024-02-05"
 
 import yaml
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from datetime import datetime
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from azure.storage.blob import BlobServiceClient
 
 env_file = "env"
 
@@ -75,3 +78,82 @@ class ConfigManager:
                 }
                 accounts.append(account)
         return accounts
+
+
+class StorageManager:
+    def __init__(self, account_name, account_key) -> None:
+        self.account_name = account_name
+        self.account_key = account_key  # DefaultAzureCredential()
+        self.blob_service_client = self.create_blob_client()
+
+    def create_blob_client(self):
+        account_url = f"https://{self.account_name}.blob.core.windows.net"
+        blob_service_client = BlobServiceClient(
+            account_url=account_url, credential=self.account_key
+        )
+        return blob_service_client
+
+    def download_blob(self, container_name, blob_name, file_name):
+        container_client = self.blob_service_client.get_container_client(
+            container=container_name
+        )
+        try:
+            blob_client = container_client.get_blob_client(blob_name)
+            with open(file_name, "wb") as local_file:
+                blob_data = blob_client.download_blob()
+                local_file.write(blob_data.readall())
+        except:
+            pass
+
+    def upload_blob(self, container_name, file_name, blob_name):
+        container_client = self.blob_service_client.get_container_client(
+            container=container_name
+        )
+        try:
+            with open(file_name, "rb") as data:
+                container_client.upload_blob(name=blob_name, data=data, overwrite=True)
+        except:
+            pass
+
+    def list_blobs(self, container_name):
+        blobs = []
+        container_client = self.blob_service_client.get_container_client(
+            container=container_name
+        )
+        result = container_client.list_blobs()
+        for r in result:
+            blobs.append(r.name)
+        return blobs
+
+    def delete_blob(self, container_name, blob_name):
+        container_client = self.blob_service_client.get_container_client(
+            container=container_name
+        )
+        blob_client = container_client.get_blob_client(blob_name)
+        blob_client.delete_blob()
+
+
+class VaultManager:
+    def __init__(self, key_vault_name) -> None:
+        keyvault_url = f"https://{key_vault_name}.vault.azure.net"
+        self.status = {}
+        self.keyvault_url = keyvault_url
+        self.credentials = self.get_credentials()
+        self.secret_client = self.create_secret_client()
+
+    def get_credentials(self):
+        try:
+            credentials = DefaultAzureCredential()
+        except:
+            credentials = None
+        return credentials
+
+    def create_secret_client(self):
+        return SecretClient(vault_url=self.keyvault_url, credential=self.credentials)
+
+    def get_secret(self, secret_access_key):
+        try:
+            secret = self.secret_client.get_secret(secret_access_key).value
+        except Exception as e:
+            secret = ""
+        return secret
