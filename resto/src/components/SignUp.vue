1ed8a9c23438c52d5cae72e5ebfbc56b70e3fa20
Mezni <mamezni@gmail.com>
2024-02-10 17:02:15 -0500

    work 20240210

---
+++ resto/src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <img class="logo" src="../assets/resto.jpeg" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" id="name" placeholder="Enter Name" />
+    <input type="email" id="email" placeholder="Enter Email" />
+    <input type="password" id="password" placeholder="Enter Password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  setup() {},
+};
+</script>
+
+<style>
+.logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: white;
+  cursor: pointer;
+}
+</style>
