efede1711a0713157a33b301438e091fa9f8749b
Mezni <mamezni@gmail.com>
2024-02-10 09:38:55 -0500

    work 20240210

--- analytics/README.md
+++
@@ -1,5 +0,0 @@
-docker run -it --rm -p 8888:8888 -v /home/dali/WORK/work02/analytics/notebooks:/home/jovyan/work jupyter/pyspark-notebook
-
-
-
-
--- analytics/services/generator-service/src/app/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Request(BaseModel):
-    interval_start: str
-    interval_mins: int
-    trx_count: int
