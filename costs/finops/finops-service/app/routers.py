a29d940fb44a59efa5cc65cc814afa5c64ef392a
Mezni <mamezni@gmail.com>
2024-02-24 19:07:30 -0500

    work 20240224

--- costs/finops/finops-service/app/routers.py
+++ costs/finops/finops-service/app/routers.py
@@ -1,16 +1,28 @@
 from fastapi import APIRouter, status, Depends
 from app.database import get_connection
 from app.schemas import ClientDim
+import app.repositories as repositories
 
 from datetime import datetime
 
 finops_router = APIRouter()
 
 
-@finops_router.post("/client_dim", status_code=status.HTTP_201_CREATED)
-async def create_client_dim(payload: ClientDim, conn=Depends(get_connection)):
+@finops_router.post("/client", status_code=status.HTTP_201_CREATED)
+async def create_client(payload: ClientDim, conn=Depends(get_connection)):
+    result = await repositories.create_client(payload, conn)
+    return result
+
+
+"""
+@finops_router.post("/client", status_code=status.HTTP_201_CREATED)
+async def create_client(payload: ClientDim, conn=Depends(get_connection)):
     await conn.execute(
         "INSERT INTO client_dim (client_name) VALUES ($1)",
         payload.client_name,
     )
     return {"message": "Item created successfully"}
+"""
+
+#    result = await repositories.create_moteur(
+#        session=session, payload=payload.dict())
