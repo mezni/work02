e3c63558dc4a4fbbf8af9302913c1fe5d27b55b3
Mezni <mamezni@gmail.com>
2024-02-28 15:01:30 -0500

    work 20240228

---
+++ bi_loader/processor.py
@@ -0,0 +1,19 @@
+import asyncio
+import asyncpg
+
+import pandas as pd
+
+
+db_config = {
+    "user": "finops",
+    "password": "passw0rd",
+    "database": "finops",
+    "host": "172.18.0.2",
+}
+
+
+async def main():
+    pass
+
+
+asyncio.run(main())
