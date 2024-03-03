27b1d6c3d5e45968e5fa02af9e1672c39ae572e7
Mezni <mamezni@gmail.com>
2024-03-03 10:23:04 -0500

    work 20240303

--- bi_loader/src/app/models.py
+++ bi_loader/src/app/models.py
@@ -1,19 +1,59 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, select
+from sqlalchemy import Column, Integer, BigInteger, String, ForeignKey, select
+from sqlalchemy.exc import IntegrityError, NoResultFound
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class Provider(Base):
-    __tablename__ = "provider"
+    __tablename__ = "providers"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     code = Column(String(60), unique=True, nullable=False, index=True)
     name = Column(String(255))
 
+    @classmethod
+    async def create_all(cls, db: AsyncSession, data: list):
+        result = []
+        if data:
+            try:
+                transaction = []
+                for d in data:
+                    transaction.append(cls(**d))
+                db.add_all(transaction)
 
-class Region(Base):
-    __tablename__ = "region"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    code = Column(String(60), unique=True, nullable=False, index=True)
-    name = Column(String(255))
-    provider_id = Column(Integer, ForeignKey("provider.id"), index=True)
+                await db.commit()
+            except Exception as e:
+                await db.rollback()
+                return None, str({e})
+
+            for trx in transaction:
+                await db.refresh(trx)
+                result = [
+                    {
+                        key: getattr(obj, key)
+                        for key in obj.__dict__
+                        if not key.startswith("_")
+                    }
+                    for obj in transaction
+                ]
+
+        return result, None
+
+    @classmethod
+    async def get_all(cls, db: AsyncSession):
+        result = []
+        try:
+            transaction = (await db.execute(select(cls))).scalars().all()
+            result = [
+                {
+                    key: getattr(obj, key)
+                    for key in obj.__dict__
+                    if not key.startswith("_")
+                }
+                for obj in transaction
+            ]
+        except Exception as e:
+            return None, str({e})
+
+        return result, None
--- bi_loader/src/main.py
+++ bi_loader/src/main.py
@@ -1,10 +1,27 @@
 import asyncio
-from app.database import init_db, get_session
+from app.database import create_db, get_session
+from app.models import Provider
+
+data = [
+    {"code": "aws", "name": "Amazon"},
+    {"code": "azure", "name": "Microsoft"},
+    {"code": "oci", "name": "Oracle"},
+    {"code": "snowflake"},
+]
 
 
 async def main():
-    await init_db()
-    session = await get_session()
+    await create_db()
+    async with await get_session() as session:
+
+        p, err = await Provider.create_all(session, data)
+        print(p)
+        print(err)
+
+
+#        p, err = await Provider.get_all(session)
+#        print(p)
+#        print(err)
 
 
 asyncio.run(main())
