c6b334ae06461d803b2f176ecbbe38ac0fcb77fb
Mezni <mamezni@gmail.com>
2025-11-20 17:56:48 -0500

    commit 2025/11/20

---
+++ work/services/auth-service/target/debug/deps/auth_service-0e6962e15b11ba82.d
@@ -0,0 +1,9 @@
+/home/dali/WORK/work02/work/services/auth-service/target/debug/deps/auth_service-0e6962e15b11ba82.d: src/main.rs Cargo.toml
+
+/home/dali/WORK/work02/work/services/auth-service/target/debug/deps/libauth_service-0e6962e15b11ba82.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ work/services/auth-service/target/debug/deps/auth_service-94de1f143ab6c4dd.d
@@ -0,0 +1,5 @@
+/home/dali/WORK/work02/work/services/auth-service/target/debug/deps/auth_service-94de1f143ab6c4dd.d: src/main.rs
+
+/home/dali/WORK/work02/work/services/auth-service/target/debug/deps/auth_service-94de1f143ab6c4dd: src/main.rs
+
+src/main.rs:
