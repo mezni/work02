9d845700a20cba96d3df5fc3beae2d7ed001b5d6
Mezni <mamezni@gmail.com>
2025-11-30 22:13:10 -0500

    commit 2025/11/30

--- everest/configurator-service/src/interfaces/http/mod.rs
+++ everest/configurator-service/src/interfaces/http/mod.rs
@@ -1,2 +1,8 @@
+pub mod handlers;
 pub mod health;
 pub mod openapi;
+
+// Re-export for easier access
+pub use handlers::*;
+pub use health::*;
+pub use openapi::*;
--- everest/configurator-service/src/interfaces/http/openapi.rs
+++ everest/configurator-service/src/interfaces/http/openapi.rs
@@ -1,14 +1,16 @@
 use utoipa::OpenApi;
 
-use crate::interfaces::http::health::Response;
-
 #[derive(OpenApi)]
 #[openapi(
     paths(
-        crate::interfaces::http::health::health_checker_handler
+        crate::interfaces::http::health::health_checker_handler,
+        crate::interfaces::http::handlers::get_users_handler
     ),
     components(
-        schemas(Response)
+        schemas(
+            crate::interfaces::http::health::Response,
+            crate::interfaces::http::handlers::User
+        )
     ),
     tags(
         (name = "Configurator Service", description = "Manage resources")
