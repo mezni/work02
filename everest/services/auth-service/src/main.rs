66c9ce86ce3a95beefeaaf56f538c6ef93e4e06f
Mezni <mamezni@gmail.com>
2025-12-24 09:23:25 -0500

    commit 2025/12/24

--- everest/services/auth-service/src/main.rs
+++ everest/services/auth-service/src/main.rs
@@ -1,14 +1,3 @@
-use anyhow::Context;
-use auth_service::core::{config::Config, logging};
-
-#[actix_web::main]
-async fn main() -> anyhow::Result<()> {
-    let config = Config::from_env();
-    logging::init_logging(&config.log_level);
-
-    auth_service::run()
-        .await
-        .context("Application server crashed")?;
-
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
